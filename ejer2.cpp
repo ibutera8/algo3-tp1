4f34ec5412511fb397c2544708fed2a7ae84e907
ibutera8 <ibutera@dc.uba.ar>
2023-09-04 16:08:27 -0300

    wrong answer del 2

--- ejer2.cpp
+++ ejer2.cpp
@@ -26,56 +26,6 @@ int calcularDescontento(vector<pair<int,int>> &l,vector<pair<float,int>> &r, int
     return d;
 }
 
-/* 
-//l vector de parciales, t tiempo acumulado, d descontento acumulado
-void optimizarCorreciones(vector<pair<int,int>> & l ,int t, int d){
-    int prueba2 = mejor_descontento;
-    int largo_l = l.size();
-    if (l.size() == 0) //no hay nada por corregir, ya termine
-    {
-        if (d < mejor_descontento)
-        {
-            mejor_descontento = d % (10^9+7); 
-        }  
-    }
-    for (size_t i = 0; i < l.size(); i++) //para cada estudiante, considero corregir su examen
-    {
-        int demora_examen_actual = (l[i].first) % (10^9+7);
-        t += demora_examen_actual;
-        //int dint calcularDescontento(vector<pair<int,int>> &l,vector<pair<int,int>> &r, int t, int d){
-    for (int i = 0; i < r.size() ; i++)
-    {
-        int posicion = r[i].second; //posicion del parcial que tiene peor ratio tiempo/coeficiente
-        int demora_examen_actual = l[posicion].first; //demora que trae ese examen
-        for (int j = 0; j < l.size(); j++)
-        {   
-            d += demora_examen_actual * l[j].second; 
-        }
-        l[posicion].first = 0;
-    }
-    return d;
-}
-escontento_estudiante = demora_examen_actual * l[i][1];
-        //d+= descontento_estudiante;
-        for (size_t j = i; j < l.size() ; j++) //sumo el descontento ocasionado por la espera del resto (que todavÃ­a no recibieron su examen)
-        {
-            d += (l[j].second * demora_examen_actual) % (10^9+7); 
-        }
-        //recursion: sigo corrigiendo teniendo en cuenta que acabo de corregir a este:
-        pair<int,int> examen_actual = l[i];
-        //caso en el que lo corregi:
-        vector<pair<int,int>> l_anterior = l;   
-        l.erase(l.begin() + i);
-        optimizarCorreciones(l, t, d);
-        //para el caso en el que no lo corregi, sigo haciendo backtracking: 
-        l = l_anterior;
-        //l[i-1] = examen_actual;
-
-    }
-    
-}          
- */
-
 struct criterio {
     bool operator()(const std::pair<int,int> &left, const std::pair<int,int> &right) {
         return (left.first > right.first) || (left.first == right.first && left.second > right.second);
@@ -99,25 +49,30 @@ vector<pair<float,int>> obtener_Peores(vector<pair<int,int>> l){
 }
 
 int main(){
- 
-    mejor_descontento = std::numeric_limits<int>::max();
-    int e;    
-    cin >> e;
-    vector<pair<int,int>> l(e, make_pair(0,0));
-    //para cada estudiante, ingresame su demora 
-    for(int j = 0; j < e; j++){
-        int demora_e;
-        cin >> demora_e;            
-        l[j].first = demora_e; 
-        }
-        //para cada estudiante, ingresame su coeficiente
-    for (int j = 0; j < e; j++){
-        int coeficiente_e;
-        cin >> coeficiente_e;
-        l[j].second = coeficiente_e;
-    } 
-    vector<pair<float, int>> r = obtener_Peores(l);
-    //optimizarCorreciones(l, 0,0);
-    cout << "resultado es " << calcularDescontento(l, r, 0, 0) <<" "; 
-    return 1;
+    int cantidad_casos;
+    cin >> cantidad_casos;
+    for (int i = 0; i < cantidad_casos; i++) {
+        mejor_descontento = std::numeric_limits<int>::max();
+        int e;    
+        cin >> e;
+        vector<pair<int,int>> l(e, make_pair(0,0));
+        //para cada estudiante, ingresame su demora 
+        for(int j = 0; j < e; j++){
+            int demora_e;
+            cin >> demora_e;            
+            l[j].first = demora_e; 
+            }
+            //para cada estudiante, ingresame su coeficiente
+        for (int j = 0; j < e; j++){
+            int coeficiente_e;
+            cin >> coeficiente_e;
+            l[j].second = coeficiente_e;
+        } 
+        vector<pair<float, int>> r = obtener_Peores(l);
+        //optimizarCorreciones(l, 0,0);
+        cout << calcularDescontento(l, r, 0, 0) << endl; 
+
+    }
+  
+    return 0;
 }
