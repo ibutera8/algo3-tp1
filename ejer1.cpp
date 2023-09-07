25f3d8fe402b00e0ff0327e6e0f1752059ce4ecf
ibutera8 <90341068+ibutera8@users.noreply.github.com>
2023-09-07 09:45:05 -0300

    ejer1 pasa los tests de catedra, queda pasarselo al juez

--- ejer1.cpp
+++ ejer1.cpp
@@ -1,98 +1,101 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <limits>
 
 using namespace std;
 
 int caminoMinimo; 
 int caminoMaximo;
-int largoActual;
-int pasosTotales;
+int inf = std::numeric_limits<int>::max();
+//int largoActual;
+//int pasosTotales;
 
 
 
-bool senderos(vector<vector<pair<char, int>>> &tablero, int i, int j){
+bool senderos(vector<vector<pair<char, int>>> &tablero, int i, int j, int l){    
+    if(l == 12 && i == 3 && j == 3) cout << "ACAAAAAAA" << caminoMaximo << endl;
+    
     int n = tablero.size();
     int m = tablero[0].size();
     bool res = false;
-    
-    if(largoActual == 16 || caminoMaximo == 16 || caminoMinimo == 16){
-        cout << i << " " << j << endl;
-    }
+
     //caso base: llegue a un final invalido (x/ej un costado del tablero o un #)
-    if((i==n && j!=m) || (i!=n && j==m)) return false;
+    if((i>=n && j<m) || (i<n && j>=m)) return false;
     if(i < 0 || j < 0) return false;
+    if (l > n*m) return false;
     if(tablero[i][j].first == *"#") return false;
-    if (pasosTotales > n*m) return false;
 
     //caso base: llegue al final del tablero
     if(i==n-1 && j==m-1) {
-        caminoMaximo = max(caminoMaximo, largoActual);
-        caminoMinimo = min(caminoMinimo, largoActual);
+        if (l < caminoMinimo) {
+            caminoMinimo = l;
+        }
+        if (l > caminoMaximo) {
+            caminoMaximo = l;
+        }
         return true;
     }
-    
     //llamados recursivos
-    // HAY QUE REESCBRIBIR LOS LLAMADOS RECURSIVOS
-    int largoViejo = largoActual;
-    largoActual++;
-    pasosTotales++;
-    //char signoAnterior = tablero[i][j].first; 
-    //tablero[i][j].first = *"#";
-    if(tablero[i][j].first == *"L"){
-        int rotVieja = tablero[i][j].second;
+    //largoActual++;
+    char signoAnterior = tablero[i][j].first; 
+    tablero[i][j].first = *"#";
+    int rotacionAnterior = tablero[i][j].second;
+    bool norte, sur, este, oeste;
+    if(signoAnterior == *"L"){
         for(int k = 0; k < 4; k++){
-            //aplico la rotacion para que avance en el proximo llamado:
-            tablero[i][j].second++;
             if(tablero[i][j].second % 4 == 0){
                 //puedo moverme al N o E
-                res = res || (senderos(tablero, i, j+1) || senderos(tablero, i-1, j));
+                norte = senderos(tablero, i-1, j, l+1);
+                este = senderos(tablero, i, j+1, l+1);
+                res = res || norte || este;
             } else if(tablero[i][j].second % 4 == 1){
                 //puedo moverme al S o E
-                res = res || (senderos(tablero, i+1, j) || senderos(tablero, i, j+1));
+                sur = senderos(tablero, i+1, j, l+1);
+                este = senderos(tablero, i, j+1, l+1);
+                res = res || sur || este;
             } else if(tablero[i][j].second % 4 == 2){
                 //puedo moverme al O o S
-                res = res || (senderos(tablero, i, j-1) || senderos(tablero, i+1, j));
+                sur = senderos(tablero, i+1, j, l+1);
+                oeste = senderos(tablero, i, j-1, l+1);
+                res = res || sur || oeste; 
             } else if(tablero[i][j].second % 4 == 3){
                 //puedo moverme al O o N
-                res = res || (senderos(tablero, i, j-1) || senderos(tablero, i-1, j));
+                norte = senderos(tablero, i-1, j, l+1);
+                oeste = senderos(tablero, i, j-1, l+1);
+                res = res || norte || oeste;
             }
-        }
-        //backtracking
-        //tablero[i][j].first = signoAnterior;
-        tablero[i][j].second = rotVieja;
-
-    } else if (tablero[i][j].first == *"I") {
-        int rotVieja = tablero[i][j].second;
-        //char signoAnterior = tablero[i][j].first; 
-        //tablero[i][j].first = *"#";
-        for(int k = 0; k < 2; k++){
             //aplico la rotacion para que avance en el proximo llamado:
             tablero[i][j].second++;
+        }
+    } else if (signoAnterior == *"I") {
+        for(int k = 0; k < 2; k++){ 
             if(tablero[i][j].second % 2 == 0){
                 //puedo moverme al N o S
-                res = res || (senderos(tablero, i+1, j) || senderos(tablero, i-1, j));
+                norte = senderos(tablero, i+1, j, l+1);
+                sur = senderos(tablero, i-1, j, l+1);
+                res = res || norte || sur;
             } else if(tablero[i][j].second % 2 == 1){
                 //puedo moverme al O o E
-                res = res || (senderos(tablero, i, j-1) || senderos(tablero, i, j+1));
+                este = senderos(tablero, i, j+1, l+1);
+                oeste = senderos(tablero, i, j-1, l+1);
+                res = res || este || oeste;
             }
+            //aplico la rotacion para que avance en el proximo llamado:
+            tablero[i][j].second++;
         }
-        //backtracking
-        //tablero[i][j].first = signoAnterior;
-        tablero[i][j].second = rotVieja;
-
-    } else if (tablero[i][j].first == *"+") {
+    } else if (signoAnterior == *"+") {
         //en este caso es indistinto rotar
-        //char signoAnterior = tablero[i][j].first; 
-        //tablero[i][j].first = *"#";
-        res = res || ((senderos(tablero, i+1, j) || senderos(tablero, i-1, j)) || (senderos(tablero, i, j-1) || senderos(tablero, i, j+1)));
-        //tablero[i][j].first = signoAnterior;
+        sur = senderos(tablero, i+1, j, l+1);
+        norte = senderos(tablero, i-1, j, l+1);
+        este = senderos(tablero, i, j+1, l+1);
+        oeste = senderos(tablero, i, j-1, l+1);
+        res = res || sur || norte || este || oeste;
     }
 
     //backtracking
-    largoActual = largoViejo;
-    pasosTotales--;
+    tablero[i][j].first = signoAnterior;
+    tablero[i][j].second = rotacionAnterior;
+    //largoActual--;
     return res;
 
 }
@@ -101,10 +104,9 @@ int main(){
     int cant_tests;
     cin >> cant_tests;
     for (int k = 0; k < cant_tests; k++){
-        caminoMinimo = std::numeric_limits<int>::max(); 
-        caminoMaximo = 0;
-        largoActual = 0;
-        pasosTotales = 0;
+        caminoMinimo = inf; 
+        caminoMaximo = -inf;
+        //largoActual = 0;
         int filas, columnas;
         cin >> filas;
         cin >> columnas;
@@ -117,7 +119,7 @@ int main(){
             }
         }
 
-        if (senderos(tablero, 0,0)){
+        if (senderos(tablero, 0, 0, 0)){
             cout << "POSIBLE " << caminoMinimo << " " << caminoMaximo;
         } else {
             cout << "IMPOSIBLE";
